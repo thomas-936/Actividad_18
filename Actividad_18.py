aa5484dd32cea10ed526df681f4790c5f8493d50
tomas <tomas27@gmail.com>
2025-08-24 16:06:35 -0600

    4

--- Actividad_18.py
+++ Actividad_18.py
@@ -6,24 +6,28 @@ class Categoria:
         self.nombre = nombre
 
 class Producto:
-    def __init__(self, IDproducto, nombre, precio, IDcategoria, total_compras, total_ventas, stock):
+    def __init__(self, IDproducto, nombre, precio, IDcategoria, total_compras=0, total_ventas=0, stock=0):
         self.IDproducto = IDproducto
         self.nombre = nombre
         self.precio = precio
         self.IDcategoroia = IDcategoria
         self.total_compras = total_compras
-        self.total_vetas = total_ventas
+        self.total_ventas = total_ventas
         self.stock = stock
 
 
 class Gestion_prodcutos:
     def __init__(self):
-        self.categoria = {}
-        self.producto = {}
+        self.categorias = {}
+        self.productos = {}
 
-    def agragar_categoria(self, IDcategoria, nombre_categoria):
+    def agregar_categoria(self, IDcategoria, nombre_categoria):
         self.categoria[IDcategoria] = Categoria(IDcategoria, nombre_categoria)
         print("Categoria agregada con éxito")
+
+    def agregar_productos(self,id_producto, nombre_producto, precio, id_categoria, stock):
+        self.productos[id_producto] = Producto(id_producto, nombre_producto, precio, id_categoria, stock)
+        print("Producto ingresado con éxito... ")
 
 gestion = Gestion_prodcutos()
 
@@ -48,4 +52,15 @@ class Menu:
                 case 1:
                     id_categoria = input("Ingrse el ID de la categoria: ")
                     nombre_categoria = input("Ingrse el nombre de la categoria: ")
-                    gestion.agragar_categoria(id_categoria, nombre_categoria)
+                    gestion.agregar_categoria(id_categoria, nombre_categoria)
+
+                case 2:
+                    id_producto = input("Ingrese el ID del producto: ")
+                    nombre_producto = input("Ingrese el nombre del producto: ")
+                    precio_producto = input("Ingrese el precio del producto: ")
+                    id_cat = input("Ingrese el ID de la categoria: ")
+                    if id_cat not in gestion.categorias:
+                        print("ERROR, primero agrege la categoria...")
+                    else:
+                        stock = int(input("Ingrese el stock inicial: "))
+                        gestion.agregar_productos(id_producto, nombre_producto, precio_producto, id_cat, stock=stock)
