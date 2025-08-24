fcfb1ef74e5e288a4b6c6ce884014c591b5c3995
tomas <tomas27@gmail.com>
2025-08-24 12:43:46 -0600

    2

--- Actividad_18.py
+++ Actividad_18.py
@@ -1,1 +1,17 @@
 print("Actividad 18")
+
+class Categoria:
+    def __init__(self, IDcategoria, nombre):
+        self.IDcategoria = IDcategoria
+        self.nombre = nombre
+
+class Producto:
+    def __init__(self, IDproducto, nombre, precio, IDcategoria, total_compras, total_ventas, stock):
+        self.IDproducto = IDproducto
+        self.nombre = nombre
+        self.precio = precio
+        self.IDcategoroia = IDcategoria
+        self.total_compras = total_compras
+        self.total_vetas = total_ventas
+        self.stock = stock
+
